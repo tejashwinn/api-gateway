f6b6e7e08eca7402b392b956a4e040307b8c961e
tejashwin.u <tejashwin5104@gmail.com>
2023-11-26 12:46:31 +0530

    Added API Gateway

---
+++ src/main/java/com/tejashwin/api/gateway/config/GatewayConfig.java
@@ -0,0 +1,20 @@
+package com.tejashwin.api.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("service1",
+                        r -> r.path("/admin/**")
+                                .filters(f -> f.rewritePath("/admin/(?<segment>.*)", "/$\\{segment}"))
+                                .uri("lb://admin-service"))
+                .build();
+    }
+}
