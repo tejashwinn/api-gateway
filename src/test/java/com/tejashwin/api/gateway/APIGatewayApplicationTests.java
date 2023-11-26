6b2b34ab0ddf0bbc721fe11df59e41a6556d81d4
tejashwin.u <tejashwin5104@gmail.com>
2023-11-26 12:22:49 +0530

    Init

---
+++ src/test/java/com/tejashwin/api/gateway/APIGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejashwin.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APIGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
